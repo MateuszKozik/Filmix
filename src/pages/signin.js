6b6887fe33d807c6d8106e72761784d7e2c55e59
Mateusz Kozik <mateuszkozik100@gmail.com>
2021-02-18 20:34:29 +0100

    Create signin component and logic

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,3 +1,71 @@
+import { useState, useContext } from "react";
+import { FirebaseContext } from "../context/firebase";
+import { FooterContainer } from "../containers/footer";
+import HeaderContainer from "../containers/header";
+import { Form } from "../components";
+import * as ROUTER from "../constants/routes";
+import { useHistory } from "react-router-dom";
+
 export default function Signin() {
-	return <p>Hello from the Signin</p>;
+	const history = useHistory();
+	const { firebase } = useContext(FirebaseContext);
+	const [emailAddress, setEmailAddress] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+
+	const isInvalid = password === "" || emailAddress === "";
+	const handleSignin = (event) => {
+		event.preventDefault();
+
+		firebase
+			.auth()
+			.signInWithEmailAndPassword(emailAddress, password)
+			.then(() => {
+				history.push(ROUTER.BROWSE);
+			})
+			.catch((error) => {
+				setEmailAddress("");
+				setPassword("");
+				setError(error.message);
+			});
+	};
+
+	return (
+		<>
+			<HeaderContainer>
+				<Form>
+					<Form.Title>Sign In</Form.Title>
+					{error && <Form.Error>{error}</Form.Error>}
+
+					<Form.Base onSubmit={handleSignin} method="POST">
+						<Form.Input
+							placeholder="Email address"
+							value={emailAddress}
+							onChange={({ target }) => {
+								setEmailAddress(target.value);
+							}}
+						/>
+						<Form.Input
+							type="password"
+							value={password}
+							autoComplete="off"
+							placeholder="Password"
+							onChange={({ target }) => setPassword(target.value)}
+						/>
+						<Form.Submit disabled={isInvalid} type="submit">
+							Sign In
+						</Form.Submit>
+					</Form.Base>
+
+					<Form.Text>
+						New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+					</Form.Text>
+					<Form.TextSmall>
+						Register to watch Your favourite movies!
+					</Form.TextSmall>
+				</Form>
+			</HeaderContainer>
+			<FooterContainer></FooterContainer>
+		</>
+	);
 }
