d07750234338dcb41ad4028cd032ea8b3626130c
Mateusz Kozik <mateuszkozik100@gmail.com>
2021-02-19 12:11:28 +0100

    Create profiles component

---
+++ src/containers/profiles.js
@@ -0,0 +1,29 @@
+import * as ROUTES from "../constants/routes";
+import { Header } from "../components";
+import { Profiles } from "../components";
+
+export function SelectProfileContainer({ user, setProfile }) {
+	return (
+		<>
+			<Header bg={false}>
+				<Header.Frame>
+					<Header.Logo to={ROUTES.HOME} src="" alt="Filmix" />
+				</Header.Frame>
+			</Header>
+
+			<Profiles>
+				<Profiles.Title>Who's watching?</Profiles.Title>
+				<Profiles.List>
+					<Profiles.User
+						onClick={() =>
+							setProfile({ displayName: user, photoURL: user.photoURL })
+						}
+					>
+						<Profiles.Picture src={user.photoURL} />
+						<Profiles.Name>{user.displayName}</Profiles.Name>
+					</Profiles.User>
+				</Profiles.List>
+			</Profiles>
+		</>
+	);
+}
