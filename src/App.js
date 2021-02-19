711795b07bc45b8dadf576f873e8a165a5b82e18
Mateusz Kozik <mateuszkozik100@gmail.com>
2021-02-19 10:21:53 +0100

    Create secured routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,43 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import * as ROUTES from "./constants/routes";
 import { Home, Browse, Signin, Signup } from "./pages";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 
 export default function App() {
+	const user = {};
+
 	return (
 		<Router>
-			<Route exact path={ROUTES.SIGN_IN} component={Signin} />
-			<Route exact path={ROUTES.SIGN_UP} component={Signup} />
-			<Route exact path={ROUTES.BROWSE} component={Browse} />
-			<Route exact path={ROUTES.HOME} component={Home} />
+			<IsUserRedirect
+				user={user}
+				loggedInPath={ROUTES.BROWSE}
+				path={ROUTES.SIGN_IN}
+				exact
+			>
+				<Signin />
+			</IsUserRedirect>
+
+			<IsUserRedirect
+				user={user}
+				loggedInPath={ROUTES.BROWSE}
+				path={ROUTES.SIGN_UP}
+				exact
+			>
+				<Signup />
+			</IsUserRedirect>
+
+			<ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+				<Browse />
+			</ProtectedRoute>
+
+			<IsUserRedirect
+				user={user}
+				loggedInPath={ROUTES.BROWSE}
+				path={ROUTES.HOME}
+				exact
+			>
+				<Home />
+			</IsUserRedirect>
 		</Router>
 	);
 }
